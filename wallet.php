e035a1ca845c2c83e8aae87511b3662dec4196a8
mmp4k <marcin@pilsniak.pl>
2017-05-14 12:33:45 +0200

    Refactor wallet.php to use transactions.php file

--- wallet.php
+++ wallet.php
@@ -3,41 +3,19 @@
 $currentValue = 94.9;
 $commissionOut = 5.0;
 
-include 'vendor/autoload.php';
+include_once 'vendor/autoload.php';
 
-$wallet = new \Domain\ETFSP500\Wallet();
+/** @var \Domain\ETFSP500\Wallet $wallet */
+$wallet = include 'transactions.php';
 
-$transaction = new \Domain\ETFSP500\WalletTransaction(
-    DateTime::createFromFormat('d.m.Y', '10.04.2017'),
-        8, 96.7, 5.0);
-$wallet->addTransaction($transaction);
+foreach ($wallet->getTransactions() as $i => $transaction) {
+    $numTransaction = $i+1;
 
-echo "--- First transaction ---\n";
-echo "Profit: " . $transaction->profit($currentValue, $commissionOut) . "\n";
-echo "Bought value: " . $transaction->boughtValue() . "\n";
-echo "Current value: " . $transaction->currentValue($currentValue, $commissionOut) . "\n";
-
-$transaction = new \Domain\ETFSP500\WalletTransaction(
-    DateTime::createFromFormat('d.m.Y', '19.04.2017'),
-    9, 95.0, 5.0
-);
-$wallet->addTransaction($transaction);
-
-echo "--- Second transaction ---\n";
-echo "Profit: " . $transaction->profit($currentValue, $commissionOut) . "\n";
-echo "Bought value: " . $transaction->boughtValue() . "\n";
-echo "Current value: " . $transaction->currentValue($currentValue, $commissionOut) . "\n";
-
-$transaction = new \Domain\ETFSP500\WalletTransaction(
-    DateTime::createFromFormat('d.m.Y', '12.05.2017'),
-    13, 94.9, 5.0
-);
-$wallet->addTransaction($transaction);
-
-echo "--- Third transaction ---\n";
-echo "Profit: " . $transaction->profit($currentValue, $commissionOut) . "\n";
-echo "Bought value: " . $transaction->boughtValue() . "\n";
-echo "Current value: " . $transaction->currentValue($currentValue, $commissionOut) . "\n";
+    echo "--- {$numTransaction}. transaction ---\n";
+    echo "Profit: " . $transaction->profit($currentValue, $commissionOut) . "\n";
+    echo "Bought value: " . $transaction->boughtValue() . "\n";
+    echo "Current value: " . $transaction->currentValue($currentValue, $commissionOut) . "\n";
+}
 
 echo "--- Summary ---\n";
 echo "Profit: " . $wallet->profit($currentValue, $commissionOut) . "\n";
